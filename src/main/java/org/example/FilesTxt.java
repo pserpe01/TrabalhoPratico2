7d1cb90e53dcc97917b9b420d603d2c3c43960cd
Pedro Serpe <pserpe01@hotmail.com>
2023-12-27 18:59:36 +0000

    Update na class FilesTxt
    Está a ler o ficheiro e a imprimir;
    Foi criada a class "firstValue" para ir buscar a dimensão da raiz;

--- src/main/java/org/example/FilesTxt.java
+++ src/main/java/org/example/FilesTxt.java
@@ -4,37 +4,73 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class FilesTxt {
     private String nomeFicheiro;
     private String caminhoFicheiro;
 
-    public FilesTxt() {
+    public FilesTxt(String nomeFicheiro) {
         this.caminhoFicheiro = "..\\TrabalhoPratico2\\testes";
+        this.nomeFicheiro = nomeFicheiro;
     }
 
-    public void readFile(String nomeFicheiro){
+    public int[][] populateMatrix(){
+        int[][] matrix = new int[0][0];
+
         try {
-            File arquivo = new File(caminhoFicheiro + "\\" + nomeFicheiro + ".txt");
-            FileReader fr = new FileReader(arquivo);
+            File file = new File(caminhoFicheiro + "\\" + nomeFicheiro + ".txt");
+            Scanner scanner = new Scanner(file);
+
+            scanner.nextLine();
+
+            int rows = firstValue();
+            int columns = firstValue();
+            matrix = new int[rows][columns];
+
+            for(int i = 0; i < matrix.length; i++){
+                for(int j = 0; j < matrix.length; j++){
+                    matrix[i][j] = scanner.nextInt();
+                }
+            }
+
+
+            for(int i = 0; i < rows; i++){
+                for(int j = 0; j < columns; j++){
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("O arquivo não foi encontrado");
+        }
+        return matrix;
+    }
+
+    private int firstValue(){
+        int first = -1;
+
+        try {
+            File file = new File(caminhoFicheiro + "\\" + nomeFicheiro + ".txt");
+            FileReader fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr);
 
-            //Variável para armazenar linha a linha do ficheiro
-            String linha;
-
-            //Leitura do arquivo
-            while((linha = br.readLine()) != null) {
-                System.out.println(linha);
+            String firstRow = br.readLine();
+            if(firstRow != null){
+                String[] values = firstRow.split(" ");
+                if (values.length > 0){
+                    first = Integer.parseInt(values[0]);
+                }
             }
 
             br.close();
             fr.close();
-
-        } catch (IOException e) {
+        } catch (IOException e){
             e.printStackTrace();
         }
+
+        return first;
     }
-
-
-
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,25 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        FilesTxt f1 = new FilesTxt();
+        FilesTxt f1 = new FilesTxt("ex5");
+        FilesTxt f2 = new FilesTxt("ex6");
+        FilesTxt f3 = new FilesTxt("ex7");
+        FilesTxt f4 = new FilesTxt("ex8");
+        FilesTxt f5 = new FilesTxt("ex9");
+        FilesTxt f6 = new FilesTxt("ex10");
+        FilesTxt f7 = new FilesTxt("sp11");
+        FilesTxt f8 = new FilesTxt("uk12");
+        FilesTxt f9 = new FilesTxt("ex13");
+        FilesTxt f10 = new FilesTxt("burma14");
+        FilesTxt f11 = new FilesTxt("lau15");
+        FilesTxt f12 = new FilesTxt("ulysses16");
+        FilesTxt f13 = new FilesTxt("gr17");
+        FilesTxt f14 = new FilesTxt("ulysses22");
+        FilesTxt f15 = new FilesTxt("gr24");
+        FilesTxt f16 = new FilesTxt("fri26");
+        FilesTxt f17 = new FilesTxt("dantzig42");
+        FilesTxt f18 = new FilesTxt("att48");
 
-        f1.readFile("ex5");
+        f1.populateMatrix();
     }
 }
